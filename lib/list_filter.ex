73dc5c85db8b74c0665b72acdb93a47006d4c62a
CodeAkio <siqueira_2004@hotmail.com>
2021-09-20 03:30:10 -0300

    Resolve challenge

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,4 +1,28 @@
 defmodule ListFilter do
-  def call do
+  require Integer
+
+  @spec call([String]) :: number
+  def call(list) do
+    list
+    |> filter_number()
+    |> filter_odd()
+    |> length()
+  end
+
+  defp filter_number(list) do
+    list
+    |> Enum.map(fn elem -> is_number?(elem) end)
+    |> Enum.filter(fn elem -> elem end)
+  end
+
+  defp is_number?(text) do
+    case Integer.parse(text) do
+      {num, ""} -> num
+      _ -> false
+    end
+  end
+
+  defp filter_odd(list) do
+    Enum.filter(list, fn elem -> Integer.is_odd(elem) end)
   end
 end
